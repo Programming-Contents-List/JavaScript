1af831d9b2f53ef96f168d7f45e7b508d3463df8
4BFC <4bee.code@gmail.com>
2025-09-27 19:09:53 +0900

    🔥: main에 불필요한 코드 제거

--- js/es5/object/constructor/dog.js
+++
@@ -1,7 +0,0 @@
-// ESLint나 Prettier가 생성자 함수를 클래스로 변환하라고 제안
-function Dog(){
-    this.name = "dog";
-    this.age = 1;
-}
-
-export default Dog;
--- js/es5/object/function/memory/cache/createCache.js
+++
@@ -1,9 +0,0 @@
-function createCache(){
-    // default value
-    const key = "test";
-    const value = "0xtest";
-    const cache = new Map();
-    return cache.set(key, value);
-}
-
-export default createCache;
