96dfe0126280177a0e9012675941abce3012d5ee
4Bee <4bee.code@gmail.com>
2025-10-01 08:30:16 +0900

    📃: generateElement.js 업데이트

--- js/es5/section2/object/literal/memory/generateElement.js
+++ js/es5/section2/object/literal/memory/generateElement.js
@@ -6,6 +6,7 @@ import createCache from "../../function/memory/createCache.js";
  * get은 자기 자신의 key값으로 캐시에 접근 할 수 있게 한다.
  * 여기서 다시 set을 하지 않는 이상, value와 key는 변경되지 않는다.
  * 그렇다면 generateElement에서 set과 get이 적절하게 역할 분류가 된 것인가?
+ * 해당 코드의 역할이 무엇인가? 요소를 생성하는 것 아닌까? 그렇다면, key와 value값을 가지고 있으면 된다. 즉, cache가 내부에서 동작 해야하는 이유가 무엇인가?
  */
 
 const generateElement = {
