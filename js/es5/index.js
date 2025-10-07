7b35846e72a68fa81fdbed2434742e513a1e9ec9
4BFC <4bee.code@gmail.com>
2025-10-07 23:49:56 +0900

    ✨: 팩토리를 사용해서 generateElement 코드에 create 함수 추가
    - 과거 generateElement를 기반으로 도식화 구현
    - Factory, Singleton 개념 문서화

--- js/es5/index.js
+++ js/es5/index.js
@@ -1,53 +1,60 @@
 import createCache from "./section2/object/function/memory/createCache.js";
-import MyConsole from "./section2/object/literal/console.js";
+/** MyConsole은 Symbol은 보여줄 수 없게 되어 있어서 발생하는 문제.  */
+// import MyConsole from "./section2/object/literal/console.js";
 import generateElement from "./section2/object/literal/memory/generateElement.js";
 import { getGlobalCache, setGlobalCache, clearGlobalCache, callAllGlobalCacheEntries, getGlobalCacheSize, getGlobalCacheKeys } from "./section2/object/function/memory/createCache.js";
 
 /**
- * 캐시로 element를 생성하지만 각 요소 내부에서만 key를 생성하고 저장만하고 있다. 따라서 전역 캐시가 없어서 listup을 할 수 없다.
+ * 힙에 맞춰서 캐시 저장 - 팩토리 패턴 사용
  */
 
-MyConsole.log("Hello World");
-MyConsole.log(createCache("key", "value").get("key"));
+// 첫 번째 엘리먼트 생성
+const element1 = generateElement.create();
+element1.initialize("Testvalue1");
+element1.key = "Testkey1";  // 외부에서 key 값을 변경, 은닉화 위반
+element1.set();
+// element1.key = "Testkey1";  // cache에 저장 되지 않은 손실된 heap 주소의 key 값, cache는 기존에 존재하는 key 값을 참조하고 있다. 즉, heap과 cache의 싱크가 안맞는다.
+console.log("Element1 - 키:", element1.key, "값:", element1.get());
 
-generateElement.set();
-// generateElement.setValue("Testvalue");
-console.log("---");
-MyConsole.log(generateElement.initialize("Testvalue"));
-MyConsole.log(generateElement.key);
-// const test = generateElement.key = "newKey";
-// MyConsole.log(test);
-MyConsole.log(generateElement.get());
+// 두 번째 엘리먼트 생성 (새로운 힙 주소)
+const element2 = generateElement.create();
+element2.initialize("Testvalue2");
+element2.set();
+console.log("Element2 - 키:", element2.key, "값:", element2.get());
 
-MyConsole.log("---");
+// 세 번째 엘리먼트 생성 (또 다른 힙 주소)
+const element3 = generateElement.create();
+element3.initialize("Testvalue3");
+element3.set();
+console.log("Element3 - 키:", element3.key, "값:", element3.get());
 
-/**
- * 글로벌로 캐시를 저장할 수 있는 코드 구현
+// 캐시 전체 확인
+console.log("=== 캐시 전체 정보 ===");
+console.log("캐시 크기:", getGlobalCacheSize());
+console.log("모든 키:", getGlobalCacheKeys());
+console.log("모든 엔트리:", JSON.stringify(callAllGlobalCacheEntries(), null, 2));
+// console.log('RAW entries =', Array.from(getGlobalCache().entries()));
+
+/** --------------------------
+ * 
+ * Element1 - 키: 0x0i639669 값: Testvalue1
+ * Element2 - 키: 0x0i863311 값: Testvalue2
+ * Element3 - 키: 0x0i152906 값: Testvalue3
+ * === 캐시 전체 정보 ===
+ * 캐시 크기: 3
+ * 모든 키: [ '0x0i639669', '0x0i863311', '0x0i152906' ]
+ * 모든 엔트리: [
+ *   {
+ *     "key": "0x0i639669",
+ *     "value": "Testvalue1"
+ *   },
+ *   {
+ *     "key": "0x0i863311",
+ *     "value": "Testvalue2"
+ *   },
+ *   {
+ *     "key": "0x0i152906",
+ *     "value": "Testvalue3"
+ *   }
+ * ]
  */
-generateElement.initialize("Testvalue");
-
-generateElement.set();
-const key1 = generateElement.key;
-MyConsole.log(key1);
-setGlobalCache(generateElement.key, generateElement.value);
-MyConsole.log(getGlobalCache().get(key1));
-
-generateElement.set();
-const key2 = generateElement.key;
-MyConsole.log(key2);
-setGlobalCache(generateElement.key, generateElement.value);
-MyConsole.log(getGlobalCache().get(key2));
-
-/**
- * 스택 기준으로 마지막에 들어온 값만 남는다. 즉, generateElement에서 하나하나 생성되고 있는 것이 아니다. 따라서, 이걸 generateElement가 될 때, cache에 저장이 되게 하려면 generateElement의 set 프로퍼티에서 globalCache가 저장이 되게 해야한다.
- * @todo 따라서 현재 generateElement에서 set 프로퍼티에서 globalCache에 저장이 되게 하려면, generateElement의 set 프로퍼티에서 setGlobalCache함수로 변경해야 한다.
- * @example
- * generateElement1 (싱글톤) → 같은 키 → 캐시에 덮어씌움
- * generateElement2 (싱글톤) → 같은 키 → 캐시에 덮어씌움
- * generateElement3 (싱글톤) → 같은 키 → 캐시에 덮어씌움
- */
-MyConsole.log("=== 캐시 전체 정보 ===");
-MyConsole.log(JSON.stringify(callAllGlobalCacheEntries(), null, 2));
-
-// clearGlobalCache();
-// MyConsole.log(getGlobalCache().get(key));
--- js/es5/section2/object/literal/memory/generateElement.js
+++ js/es5/section2/object/literal/memory/generateElement.js
@@ -1,36 +1,62 @@
 import createKey from "../../function/memory/createKey.js";
-import createCache from "../../function/memory/createCache.js";
-
-/**
- * set을 통해서 캐시에 값을 할당 할 수 있게 한다.
- * get은 자기 자신의 key값으로 캐시에 접근 할 수 있게 한다.
- * 여기서 다시 set을 하지 않는 이상, value와 key는 변경되지 않는다.
- * 그렇다면 generateElement에서 set과 get이 적절하게 역할 분류가 된 것인가?
- * 해당 코드의 역할이 무엇인가? 요소를 생성하는 것 아닌까? 그렇다면, key와 value값을 가지고 있으면 된다. 즉, cache가 내부에서 동작 해야하는 이유가 무엇인가?
- * 오히려, 내부에서 cache를 사용해야하는 이유는 key와 value를 한번에 관리하는 map값을 생성하려는 것이며, 나머진 메타 데이터이다.
- */
-
+// import createCache from "../../function/memory/createCache.js";
+import { setGlobalCache, getGlobalCache } from "../../function/memory/createCache.js";
 /**
  * @description 해당 코드는 initialize 프로퍼티와 set, get 프로퍼티로 동작을 한다.
+ * @remark 해당 릴터럴 객체는 이미 식별자가 존재한다.
  * @remark 해당 코드에서 문제점은 initialize 프로퍼티(리터럴 함수)와 set과의 역할 분리가 불분명 하다는 것이다. 즉, 해당 코드가 행동되는 것을 나열해 보면, 다음과 같다.
  * @example
  * 1. initialize 프로퍼티를 통해서 value를 설정한다.
  * 2. key가 생성된다.
  * 3(option). set 프로퍼티를 통해서 생성된 generateElement의 key와 value를 캐시에 저장한다.
  * 4(option). get 프로퍼티를 통해서 generateElement의 key와 value를 캐시에서 가져온다.
- * * notify-1 : 해당 generateElement는 일회성이다. 즉, index에서 key 값을 다시 호출해서 선언(재할당)을 하게 되면, 새로운 createKey가 동작하면서, heap의 값을 새로 생성하게 된다. 이로 인해서 메모리 누수가 발생할 수 있다.
- * * notify-2 : 해당 코드 key는 싱글톤으로 createKey가 한번 호출되어서 사용되고 있다. 그래서 가시적으로 같인 key 값을 호출 하디만, 메모리상 다른 공간을 사용하고 있다.
+ * * notify-1 : 해당 generateElement는 일회성이다. 즉, index에서 key 값(key: createKey 관점)을 다시 호출해서 선언(재할당)을 하게 되면, 새로운 createKey가 동작하면서, heap의 값을 새로 생성하게 된다. 이로 인해서 메모리 누수가 발생할 수 있다.
+ *   - `[참고](https://github.com/Programming-Contents-List/JavaScript/blob/087fa3c0e1dd431158b321039edf43cd45fee11c/js/es5/section2/object/literal/memory/generateElement.js#L27)`
+ *   * feedback : 여기서 오해한 부분은 generateElement.key를 호출 했다 해서 createKey함수가 동작, 실행, 호출 되면서 힙을 생성한 것이 아니라, key에 할당된 반환 값인 Map을 참조 하고 있기 때문에, `const test = generateElement.key = "newKey";`를 한다고 해서 createKey함수가 힙을 생성하는 것이 아니다.
+ *     - 따라서, 메모리 누수는 Heap의 Map에서 발생하는 것이다. 단, GC가 동작하기 때문에 메모리 누수를 걱장할 필요는 없지만, index 적으로는 데이터가 밀리는 현상이 있을 수 있다.
+ *   - 그렇다면, 왜 싱글톤으로 동작을 하는가. - 이는 객체 리터럴과 함수는 생성이 되면서, heap영역을 할당 받는데, 함수는 실행 컨테스트(ExectuionContext)가 생성되면서 메모리에 할당 되기 때문에, 팩토리로 동작을 하는 것이다.
+ * * notify-2 : 해당 코드 key는 싱글톤으로 createKey가 한번 호출되어서 사용되고 있다. 그래서 가시적으로 같인 key 값을 호출 하지만, 메모리상 다른 공간을 사용하고 있다. 이를 해결 하기 위해서는 팩토리얼로 구현을 해야한다. 즉, 싱크가 맞지 않는다.
+ *   - 싱글톤으로 하나의 객체를 참고 하고 있기 때문에 동적으로 데이터가 할당 되지 않는다. 이로인해서 팩토리를 사용해서 고유하지 않게 코드를 구현 하는 것이 맞다.
+ *   - 하지만, 내가 생각한 탈 함수형에 가깝다고 볼 수 없다. 즉, 객체 리터럴을 기반으로 함수형이 동작을 해야한다.
  * * notify-3 : 선언한 key 값이 외부에서 변경될 경우, 기존 새로 heap에 생성된 key 값의 참조가 변경된다. 이로 인해서 의도치 않은, 확인 되지 않은 값들이 생성되거나 저장 될 수 있다.
+ * @todo : generateElement가 생성됨과 동시에 힙영역에 발생한 메모리와 cache가 sync가 맞아야 한다.
+ * - 팩토리얼 적용 이후
+ * 1. create로 생성된 객체를 식별자로 관리한다.
+ * 2. initialize로 value를 설정한다.
+ * 3. set로 key와 value를 캐시에 저장한다.
+ * 4. get로 key와 value를 캐시에서 가져온다.
+ * 5(option). cache의 callAllGlobalCacheEntries로 모든 객체를 확인 할 수 있다. / createCache의 함수들을 통해서 확인 할 수 있다.
  */
 
 const generateElement = {
-    key : createKey(),
+    key : null,
     value : null,
+
+    /**
+     * @title 생성 책임
+     * @description create 프로퍼티는 새로운 객체를 생성하는 프로퍼티이다.
+     * @remark create 프로퍼티는 새로운 객체를 생성한다. 단, generateElement의 프로퍼티를 상속 받고 있어 역할 분리가 명확하지 않다. 더불어서 현재, 팩토리 기반일 뿐, 캡슐화가 되어 있지 않아서, 객체 변경 시 외부에서 접근이 가능하다.
+     * @todo 여기서 create는 과연 관심사 분리가 명확한가?
+     */
+    create: function(){
+        const instance = Object.create(this);
+        instance.key = createKey();
+        instance.initialize = function(value){
+            return this.value = value;
+        };
+        return instance;
+    },
+
+    /**
+     * @title 데이터 관리 책임(key, value)
+     * @description initialize 프로퍼티는 value를 설정하는 프로퍼티이다.
+     */
     initialize: function(value){
         return this.value = value;
     },
     /**
-     * @description set 프로퍼티는 createCache를 통해서 로컬 캐시에 값을 할당 할 수 있게 한다.
+     * @title 캐시 관리 책임
+     * @description set 프로퍼티는 createCache를 통해서 로컬 캐시에 값을 할당 할 수 있게 한다. 사실상, set 보다는 saved 또는 initialize에 가깝다고 생각 된다.
      * @remark set 프로퍼티는 자기 자신의 key와 value를 캐시에 저장한다. 다만, 로컬로 구현이 되어 있기 때문에, 외부에서 해당 캐시의 데이터 값을 확인하기에 적합하지 않으며, 어려움이 있다.
      * @returns {Map}
      */
@@ -38,11 +64,39 @@ const generateElement = {
         if(this.value === null){
             console.warn("⚠️value is null, check initialize value");
         }
-        return createCache(this.key, this.value);
+        setGlobalCache(this.key, this.value);
+        return this;
     },
+    /**
+     * @title 캐시 관리 책임
+     */
     get: function(){
-        return this.set().get(this.key);
-    }
-}
+        return getGlobalCache().get(this.key);
+    },
+    // create: function(){
+    //     return {
+    //         key : createKey(),
+    //         value : null,
+    //         initialize: function(value){
+    //             return this.value = value;
+    //         },
+    //         /**
+    //          * @description set 프로퍼티는 createCache를 통해서 로컬 캐시에 값을 할당 할 수 있게 한다.
+    //          * @remark set 프로퍼티는 자기 자신의 key와 value를 캐시에 저장한다. 다만, 로컬로 구현이 되어 있기 때문에, 외부에서 해당 캐시의 데이터 값을 확인하기에 적합하지 않으며, 어려움이 있다.
+    //          * @returns {Map}
+    //          */
+    //         set: function(){
+    //             if(this.value === null){
+    //                 console.warn("⚠️value is null, check initialize value");
+    //             }
+    //             setGlobalCache(this.key, this.value);
+    //             return this;
+    //         },
+    //         get: function(){
+    //             return getGlobalCache().get(this.key);
+    //         }
+    //     }
+    // }
+};
 
 export default generateElement;
