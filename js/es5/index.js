b9e9fab890a0aa121353d4c70c41bb1dcb557184
4BFC <4bee.code@gmail.com>
2025-11-09 22:53:53 +0900

    ✨: HOF 패턴과 IIFE 패턴 적용

--- js/es5/index.js
+++ js/es5/index.js
@@ -1,4 +1,3 @@
 import generateElement from "./section2-3/object/literal/memory/generateElement.js";
 
-var element = generateElement.initialize();
-console.log(element);
+generateElement.initialize();
--- js/es5/section2-3/object/function/elementService.js
+++ js/es5/section2-3/object/function/elementService.js
@@ -25,4 +25,26 @@ const elementService = {
     }
 }
 
-export default elementService;
+/**
+ * @title 로깅 함수 : 서비스 함수를 로깅하는 함수이다. (IIFE 패턴)
+ * @description withLogging는 서비스 함수를 로깅하는 함수이다. 서비스 함수를 로깅하는 함수이다.
+ * @remark 리터럴을 key, value로 만들어서 반환하고 컨트롤 하는 것이다.
+ * @param {Object} service - 서비스 함수
+ * @returns {Object} - 로깅된 서비스 함수
+ */
+function withLogging(service){
+    var logged = {};
+    for(var method in service){
+        logged[method] = (function(methodName, originalFn){
+            return function() {
+                console.log('[Service,', methodName, '] called:', arguments);
+                var result = originalFn.apply(this, arguments);
+                console.log('[Service,', methodName, '] returned:', result);
+                return result;
+            }
+        })(method, service[method]);
+    }
+    return logged;
+}
+
+export default withLogging(elementService);
