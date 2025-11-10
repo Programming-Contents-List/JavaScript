bf3a24d4cfd7f9aeabae3eb14b770c258e05007d
4BFC <4bee.code@gmail.com>
2025-11-10 23:31:00 +0900

    ✨: element 타입 정의
    - elementService에 element 종속 관계 구현

--- js/es5/section2-3/object/literal/element.js
+++ js/es5/section2-3/object/literal/element.js
@@ -2,8 +2,15 @@
  * @title 엘리먼트 객체
  * @description 엘리먼트 객체는 key와 value를 가지고 있는 객체이다. 해당 코드는 정적이고 정보만 가지고 있는 객체이다.
  * @remark object의 type을 담당한다.
+ * @typedef {Object} Element
+ * @property {string} key - element's key
+ * @property {string} value - element's value
  */
+
+/** @type {Element} */
 const element = {
-    key : null,
-    value : null
+    key : "",
+    value : ""
 }
+
+export default element;
