ccc2de609bffac8a4244e8a43e9cc2d8cecde93e
4BFC <4bee.code@gmail.com>
2025-12-03 23:57:12 +0900

    ✨: setValue 동작 확인 및 jsdoc 문법 적용 확인
    - elementManager, Service 레이어에 적용되어 있는 HOF의 프록시 패턴 특성으로 HOF로 한번 맵핑 되면서 jsdoc을 읽지 못하는 문제가 있어, 이를 따로 변수로 생성 후 jsdoc추가

--- js/es5/index.js
+++ js/es5/index.js
@@ -1,3 +1,7 @@
 import elementManager from "./section2-3/object/literal/memory/elementManager.js";
 
 elementManager.initialize();
+// elementManager.set("key", "value");
+elementManager.setValue("Tester");
+elementManager.get();
+elementManager.state();
