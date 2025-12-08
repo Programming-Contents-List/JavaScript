be2d5593cb38b5ccc2fa91c05c329ec520622350
4BFC <4bee.code@gmail.com>
2025-12-08 22:27:11 +0900

    ✨: cacheRepo에서 LRU 알고리즘 적용 및 구현

--- js/es5/section2-3/project/repository/cacheRepo.js
+++ js/es5/section2-3/project/repository/cacheRepo.js
@@ -1,1 +1,54 @@
-// 해당 코드에는 LRU가 동작할 `자료구조`만 존재할 뿐, 실제 동작을 수행하는 로직은 존재 하지 않는다. 그렇다면 자료 구조란 무엇인가?
+// 해당 코드에는 LRU가 동작할 `자료구조`만 존재, 그렇다면 자료 구조란 무엇인가?
+// cacheRepo는 외부에서 접근이 불가능 해야하나? IIFE 방식으로 작성해야하나?
+// B 방식으로 구현 추후에 A 방식 추가, map을 직접 구현
+
+var cacheRepo = (function(){
+    var cache = {}  //model, momory 값을 복사 - Dictionary
+    var order = []  //cache의 순위 즉, history, 앞의 값이 가장 오래됨.
+    var maxSize = 10;
+
+    function get(key){
+        if(cache.hasOwnProperty(key)){
+            removeFromOrder(key);
+            order.push(key);
+            return cache[key];
+        } else {
+            return null;
+        }
+    }
+
+    function set(key, node){
+        if(cache.hasOwnProperty(key)){
+            cache[key] = node;
+            removeFromOrder(key);
+            order.push(key);
+        } else {
+            // removeFromOrder(key);
+            if(order.length >= maxSize){    // 용량이 초과
+                var oldestKey = order.shift();
+                delete cache[oldestKey];
+            } 
+            cache[key] = node;
+            order.push(key);
+        } 
+    }
+
+    function removeFromOrder(key){
+        for(var i = 0; i < order.length; i++){
+            if(order[i] === key){
+                order.splice(i, 1);
+                break
+            }
+        }
+    }
+
+    return{
+        get: get,
+        set: set,
+        remove: removeFromOrder
+        // gcRemove: removeFromCache
+    }
+
+})();
+
+module.exports = cacheRepo;
