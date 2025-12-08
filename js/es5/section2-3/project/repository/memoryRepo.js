a3cba8fd47c3a2d247e25ddc7cfe4fee0c4a1471
4BFC <4bee.code@gmail.com>
2025-12-08 22:53:58 +0900

    ✨: pool 전체를 반환하는 함수 구현
    - pool 전체를 탐색할 수 있는 함수를 구현
    - 전체 pool 탐색을 기반으로 refCount가 0이면 제거한다.

--- js/es5/section2-3/project/repository/memoryRepo.js
+++ js/es5/section2-3/project/repository/memoryRepo.js
@@ -1,7 +1,15 @@
-import createKey from '../../object/function/memory/createKey.js';
+var createKey = require('../utils/createKey.js');
+
+/**
+ * @Todo ES5 환경 비호환 (미해결)
+ * @Todo 최적화 미완료 - getNode(address).createdAt만 저장 → 노드 전체를 저장하면 더 유연
+ * @remark SRP(S) : 단일 책임 원칙 - 메모리 저장소 역할만
+ * @remark OCP(O) : 개방-폐쇄 원칙 - △ - node 구조 변경 시 putNode 수정 필요
+ * @remark DIP(D) : 의존 역전 원칙 - △ - createKey 직접 import
+ */
 
 var memoryRepo = (function(){
-    var pool = {};
+    var pool = {};  // Dictionary
 
     function initialize(value){ // instance/create
         var address = createKey();
@@ -20,15 +28,27 @@ var memoryRepo = (function(){
         return pool[address];
     }
 
+
+    function getAllNode(){
+        // 얕은 복사를 사용하기를 추천
+        return pool;
+    }
+
+
     function putNode(address, value, lastAccess, refCount){
-        pool[address] = {
-            address: address,
-            value: value,
-            refCount: refCount,
-            createdAt: getNode(address).createdAt,
-            updatedAt: Date.now(),
-            lastAccess: lastAccess
-        }
+        // var existingNode = getNode(address).createdAt;
+        // pool[address] = {
+        //     address: address,
+        //     value: value,
+        //     refCount: refCount,
+        //     createdAt: existingNode,
+        //     updatedAt: Date.now(),
+        //     lastAccess: lastAccess
+        // }
+        pool[address].value =value;
+        pool[address].refCount = refCount;
+        pool[address].updatedAt = Date.now();
+        pool[address].lastAccess = lastAccess;
     }
 
     function delNode(address) {
@@ -41,8 +61,9 @@ var memoryRepo = (function(){
         read: getNode,
         update: putNode,
         delete: delNode,
+        readAll: getAllNode
         // readKey: getKey()
     };
 })();
 
-export default memoryRepo;
+module.exports = memoryRepo;
---
+++ js/es5/section2-3/project/service/gcService.js
@@ -0,0 +1,34 @@
+/** 
+ * memoryRepo의 모든 노드를 순회 
+ * 삭제할 때, cacheRepo의 값도 제거
+ * memoryRepo.pool 직접 접근 허용?
+ */
+
+var memoryRepo = require("../repository/memoryRepo");
+var cacheRepo = require("../repository/cacheRepo");
+
+var gcService = (function(){
+    function collect(){
+        var memoryAll = memoryRepo.readAll();
+
+        // memroyAll의 key 값만 address안에 들어간다.
+        for (var address in memoryAll){
+            // 여기서 continue는 가짜 키를 건너뛰고 다음 반복으로 넘어가라는 의도, 즉, 방해(false)되는 요소는 건너 뛰고 반복동작을 하라는 뜻이다.
+            if(!memoryAll.hasOwnProperty(address)) continue;
+            var node = memoryAll[address];
+            // 여기서 node가 false일 경우가 있나?
+            if(!node) continue;
+            // 타입 체크는 추후에 추가 예정
+            if(node.refCount === 0){
+                memoryRepo.delete(address);
+                cacheRepo.remove(address);
+            }
+        }
+    }
+
+    return{
+        collect: collect,
+    }
+})();
+
+module.exports = gcService;
