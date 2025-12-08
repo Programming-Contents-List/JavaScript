4ac6f3b62e06a5fb3d626775c6cca1ffd69e3af6
4BFC <4bee.code@gmail.com>
2025-12-08 22:59:58 +0900

    ✨: createKey를 utils디렉토리로 복제

---
+++ js/es5/section2-3/project/utils/createKey.js
@@ -0,0 +1,10 @@
+/**
+ * @description 난수로 생성된 키값 생성
+ */
+function createKey(){
+    var random = Math.random();
+    var key = '0x'+random;
+    return key.replace(".", "i").slice(0, 10);
+}
+
+module.exports = createKey;
